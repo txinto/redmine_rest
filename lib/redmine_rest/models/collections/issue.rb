3faf356a3f40cb5554024559cfeb996a0bd24baa
Dmitriy Non <non.dmitriy@gmail.com>
2016-03-04 11:23:32 +0300

    grouping by author

--- lib/redmine_rest/models/collections/issue.rb
+++ lib/redmine_rest/models/collections/issue.rb
@@ -7,6 +7,12 @@ module RedmineRest
       # Methods for issue collections
       #
       class Issue < ActiveResource::Collection
+        #
+        # GROUPING
+        #
+
+        # By project
+
         def group_by_project
           group_by(&:project)
         end
@@ -17,6 +23,20 @@ module RedmineRest
 
         def group_by_project_id
           group_by { |i| i.project.id }
+        end
+
+        # By author
+
+        def group_by_author
+          group_by(&:author)
+        end
+
+        def group_by_author_name
+          group_by { |i| i.author.name }
+        end
+
+        def group_by_author_id
+          group_by { |i| i.author.id }
         end
       end
     end

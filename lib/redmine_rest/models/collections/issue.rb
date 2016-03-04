2adb0f8cf95da4dcff70d58cf83b91761c5a5c8e
Dmitriy Non <non.dmitriy@gmail.com>
2016-03-04 11:28:00 +0300

    delete grouping by name and id of attributes. It is useless

--- lib/redmine_rest/models/collections/issue.rb
+++ lib/redmine_rest/models/collections/issue.rb
@@ -7,50 +7,16 @@ module RedmineRest
       # Methods for issue collections
       #
       class Issue < ActiveResource::Collection
-        #
-        # GROUPING
-        #
-
-        # By project
-
         def group_by_project
           group_by(&:project)
         end
-
-        def group_by_project_name
-          group_by { |i| i.project.name }
-        end
-
-        def group_by_project_id
-          group_by { |i| i.project.id }
-        end
-
-        # By author
 
         def group_by_author
           group_by(&:author)
         end
 
-        def group_by_author_name
-          group_by { |i| i.author.name }
-        end
-
-        def group_by_author_id
-          group_by { |i| i.author.id }
-        end
-
-        # By assigned_to
-
         def group_by_assigned_to
           group_by(&:assigned_to)
-        end
-
-        def group_by_assigned_to_name
-          group_by { |i| i.assigned_to.name }
-        end
-
-        def group_by_assigned_to_id
-          group_by { |i| i.assigned_to.id }
         end
       end
     end

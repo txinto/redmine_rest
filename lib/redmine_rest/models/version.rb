8e1567c5414d44f5a34ffd218f7c20ea3d928855
Dmitriy Non <non.dmitriy@gmail.com>
2016-03-20 17:11:52 +0300

    add prefix to Version and override Version.element_path

--- lib/redmine_rest/models/version.rb
+++ lib/redmine_rest/models/version.rb
@@ -10,6 +10,15 @@ module RedmineRest
     class Version < ActiveResource::Base
       self.format = :xml
       self.collection_parser = Collections::Version
+      self.prefix = '/projects/:project_id/'
+
+      #
+      # Overrides parent method.
+      # When we want to fetch one version, we need not to use prefix
+      #
+      def self.element_path(id, prefix_options = {}, query_options = nil)
+        "/versions/#{URI.parser.escape id.to_s}#{format_extension}#{query_string(query_options)}"
+      end
     end
   end
 end

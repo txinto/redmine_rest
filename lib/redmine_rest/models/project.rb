89c45f14d533662be24824f3aad9dbb34b5db1f4
Dmitriy Non <non.dmitriy@gmail.com>
2016-03-20 16:23:24 +0300

    Project has_many :trackers + include trackers in Project.find

--- lib/redmine_rest/models/project.rb
+++ lib/redmine_rest/models/project.rb
@@ -1,5 +1,6 @@
 require 'active_resource'
 
+require_relative 'tracker'
 require_relative 'collections/project'
 
 module RedmineRest
@@ -10,6 +11,24 @@ module RedmineRest
     class Project < ActiveResource::Base
       self.format = :xml
       self.collection_parser = Collections::Project
+
+      has_many :trackers, class_name: Tracker
+
+      #
+      # Adds trackers to request.
+      #
+      def self.find(scope, options = {})
+        options[:params] = {} unless options[:params]
+        params = options[:params]
+
+        if params[:include]
+          params[:include] += ',trackers'
+        else # doubling is not bad
+          params[:include] = 'trackers'
+        end
+
+        super(scope, options)
+      end
     end
   end
 end
